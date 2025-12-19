5a372410cde0e25e17572b10bed6c10b0c7c22cf
Yuri Claro <yuri.claro@outlook.com>
2025-12-19 12:31:54 -0300

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,14 @@ import { products } from "../data/products.js";
 import "./HomePage.css";
 
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <title>E-Commerce Project</title>
@@ -13,25 +21,28 @@ export function HomePage() {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
-                    {product.name}
+                  {product.name}
                 </div>
 
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                    src={`/images/ratings/rating-${product.rating.stars * 10}.png`}
+                    src={`/images/ratings/rating-${
+                      product.rating.stars * 10
+                    }.png`}
                   />
-                  <div className="product-rating-count link-primary">{product.rating.count}</div>
+                  <div className="product-rating-count link-primary">
+                    {product.rating.count}
+                  </div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">
+                  ${(product.priceCents / 100).toFixed(2)}
+                </div>
 
                 <div className="product-quantity-container">
                   <select>
