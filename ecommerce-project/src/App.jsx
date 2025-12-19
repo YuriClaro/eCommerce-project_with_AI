208f3bbcd126dc2e77744cc28eb8838ec9e1de67
Yuri Claro <yuri.claro@outlook.com>
2025-12-19 14:40:58 -0300

    Lifg cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,15 +1,26 @@
-import { Routes, Route } from 'react-router'
+import { useEffect, useState } from 'react'
 import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { Routes, Route } from 'react-router'
 import { OrdersPage } from './pages/OrdersPage'
+import { CheckoutPage } from './pages/CheckoutPage'
 import { TrackingPage } from './pages/TrackingPage'
+import axios from 'axios'
 import './App.css'
 
+
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+        axios.get("/api/cart-items").then((response) => {
+        setCart(response.data);
+    });
+  });
+  
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/tracking" element={<TrackingPage />} />
     </Routes>
